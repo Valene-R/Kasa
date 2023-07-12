14a9e1ddbbb3b31469ff76a75eeef4886de8abd9
Valene-R <valene.renaud@gmail.com>
2023-07-12 22:53:25 +0200

    feat(notFound): add integration of notfound page with a back link to the homepage

---
+++ src/components/LinkReturnHome/LinkReturnHome.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const LinkReturnHome = () => {
+    return (
+        <>
+        <NavLink to="/" className='linkReturnHome' >
+            Retourner à la page d'accueil
+        </NavLink>    
+        </>
+    );
+};
+
+export default LinkReturnHome;
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import LinkReturnHome from '../../components/LinkReturnHome/LinkReturnHome';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas</p>
-               
-        </div>
+        <>
+            <Header /> 
+            <main>         
+                <h1 className='codeError'>404</h1>
+                <p className='messageError'>Oups! La page que vous demandez n'existe pas.</p>
+                <LinkReturnHome />
+            </main>
+            <Footer />       
+        </>
     );
 };
 
