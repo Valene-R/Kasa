59ae695f46fb304b90dda7e7f996a5df41c6e234
Valène Renaud <valene.renaud@gmail.com>
2025-02-12 18:49:51 +0100

    style(design): update design to match the latest mockup

--- src/components/CollapseSection/CollapseSection.jsx
+++ src/components/CollapseSection/CollapseSection.jsx
@@ -7,8 +7,8 @@ const CollapseSection = () => {
                 aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
             <Collapse title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire
                 ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
-            <Collapse title="Service" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire
-                ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+            <Collapse title="Service" content="La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque 
+                interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et bienveillance." />
             <Collapse title="Sécurité" content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque
                 logement correspond aux critères de sécurité établis par nos services. En baissant une note aussi bien à l'hôte qu'au locataire,
                 cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité
